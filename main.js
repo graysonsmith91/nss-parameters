6b1ec55acef59a4b08e562e2bf214db6b9491a6a
Grayson Smith <graysonsmith91@gmail.com>
2022-12-12 10:10:36 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (num) => {
+    const quarter = num / 4
+    return quarter
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(4)
+
+// Log the return value
+console.log(result)
